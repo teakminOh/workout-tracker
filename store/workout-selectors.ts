91ce1c1aedd643bce5b42249bd58abcd6b766366
teakminOh <tekmin.oh2003@gmail.com>
2026-06-11 13:41:22 +0200

    feat: add workout selectors for current workout, total sets, and total volume.

---
+++ store/workout-selectors.ts
@@ -0,0 +1,9 @@
+import type { RootState } from './index';
+
+export const selectCurrentWorkout = (state: RootState) => state.workout.currentWorkout;
+
+export const selectTotalSets = (state: RootState) =>
+  selectCurrentWorkout(state)?.sets.length ?? 0;
+
+export const selectTotalVolume = (state: RootState) =>
+  selectCurrentWorkout(state)?.sets.reduce((total, set) => total + set.reps * set.weight, 0) ?? 0;
